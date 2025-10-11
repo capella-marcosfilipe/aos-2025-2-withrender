afb8c618bbe1d130c932a9001d3505856dd9440f
Marcos Filipe Capella <marcoscapella@outlook.com>
2025-10-11 19:02:17 -0300

    feat(user): find messages and tasks created the user

--- api/routes/user.js
+++ api/routes/user.js
@@ -13,6 +13,18 @@ router.get("/:userId", async (req, res) => {
   return res.status(200).send(user);
 });
 
+router.get("/:userId/messages", async (req, res) => {
+  const userId = req.params.userId;
+  const messages = await req.context.models.Message.findAll({ where: { userId } });
+  return res.status(200).send(messages);
+});
+
+router.get("/:userId/tarefas", async (req, res) => {
+  const userId = req.params.userId;
+  const tarefas = await req.context.models.Tarefa.findAll({ where: { userId } });
+  return res.status(200).send(tarefas);
+});
+
 router.post("/", async (req, res) => {
   const user = req.body;
   const createdUser = await req.context.models.User.create(user);
