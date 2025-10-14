eea7fc7d6809d744ef93379267f37db6b50bbd15
Marcos Filipe Capella <marcoscapella@outlook.com>
2025-10-14 08:55:02 -0300

    fix(cors): allowed any origin

--- api/index.js
+++ api/index.js
@@ -14,11 +14,7 @@ app.set("trust proxy", true);
 const port = process.env.PORT ?? 3000;
 
 // CORS configuration (allows any origin and an example origin)
-const corsOptions = {
-  origin: ["http://example.com", "*"],
-  optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
-};
-app.use(cors(corsOptions));
+app.use(cors());
 
 // ---------------------
 // Middleware
